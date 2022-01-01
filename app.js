38d6251acf320f175d765dba4fd03fb95c0ddb7f
JunhwanKang <wnsghks1017@naver.com>
2022-01-01 14:32:00 +0900

    login(1)

---
+++ app.js
@@ -0,0 +1,10 @@
+const loginForm = document.getElementById("login-form"); 
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function onLoginBtnClick(){
+    console.dir(loginInput.value);
+    console.log("click!!");
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
