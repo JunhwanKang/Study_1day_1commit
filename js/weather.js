0f5f1955e8276cd0a2313294e8513b68b3f6692f
JunhwanKang <wnsghks1017@naver.com>
2022-01-12 22:20:28 +0900

    날씨 API 적용

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,16 @@
+const API_KEY = "a93dcfd07f0f4ce7a4eea187c7d59ec9"
+
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longtude;;
-    console.log("You live in " ,lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url).then((response) => response.json()).then((data) => {
+        const weather = document.querySelector("#weather span:first-child"); 
+        const city = document.querySelector("#weather span:last-child"); 
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 } 
 function onGeoError(){
     alert("Cant't find you. No weather for you.")
